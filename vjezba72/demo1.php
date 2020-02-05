2ba36a6ce48a28b43f57d6e10899dabc61400199
pbiljce <57287222+pbiljce@users.noreply.github.com>
2020-02-05 20:04:40 +0100

    Add files via upload

---
+++ vjezba72/demo1.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+
+    $a = 1;
+    $b = 2;
+    $c = 3;
+
+    $_SESSION['a'] = $a;
+    $_SESSION['b'] = $b;
+    $_SESSION['c'] = $c;
+?>
---
+++ vjezba72/demo2.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+
+    echo $_SESSION['a'];    
+    echo $_SESSION['b'];
+    echo $_SESSION['c'];
+
+?>
